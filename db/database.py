2d48055fbf07b6fc3b2c088d669d320fd3a09e1c
Pedro Fernandes da Silva <paca.fs@gmail.com>
2025-02-07 15:54:09 +0000

    removed not needed extra code

--- db/database.py
+++ db/database.py
@@ -16,19 +16,9 @@ class Database:
         )
         self.cursor = self.conn.cursor()
 
-    def execute(self, query, params=()):
-        self.cursor.execute(query, params)
-        self.conn.commit()
-
-    def fetchone(self, query, params=()):
-        self.cursor.execute(query, params)
-        return self.cursor.fetchone()
-
-    def fetchall(self, query, params=()):
-        self.cursor.execute(query, params)
-        return self.cursor.fetchall()
-
     def close(self):
         self.cursor.close()
         self.conn.close()
+
+
 
